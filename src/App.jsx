627d1050ac1a7cdf00ff7b398c29f2d23fe4b471
Aditya kumar <adityakumar.email@gmail.com>
2025-01-30 16:24:48 +0530

    Hero Section deleted and some changes in index and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Footer from './Components/Footer/Footer'
-import HeroSection from './Components/HeroSection/HeroSection'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx'
 import Flights from './pages/Flights.jsx'
